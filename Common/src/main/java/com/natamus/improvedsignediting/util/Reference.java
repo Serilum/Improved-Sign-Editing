07793bd555ceedce91102d5dc6c52f8453632382
Rick South <rick@natamus.com>
2023-12-22 18:20:05 +0100

    (v1.1) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/improvedsignediting/util/Reference.java
+++ Common/src/main/java/com/natamus/improvedsignediting/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.improvedsignediting.util;
 public class Reference {
 	public static final String MOD_ID = "improvedsignediting";
 	public static final String NAME = "Improved Sign Editing";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
