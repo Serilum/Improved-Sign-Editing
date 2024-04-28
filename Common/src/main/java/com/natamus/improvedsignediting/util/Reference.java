9a63d336fc1fcebc03e48ec14c8de4f9d32721de
Rick South <rick@natamus.com>
2024-04-28 15:57:13 +0200

    Improved Sign Editing source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/improvedsignediting/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.improvedsignediting.util;
+
+public class Reference {
+	public static final String MOD_ID = "improvedsignediting";
+	public static final String NAME = "Improved Sign Editing";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
