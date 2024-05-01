540c2cb4d548d78e229f8c8ce36ca966ef96e58e
Rick South <rick@natamus.com>
2024-05-01 16:37:43 +0200

    Improved Sign Editing source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/improvedsignediting/neoforge/events/NeoForgeSignEditEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.improvedsignediting.neoforge.events;
+
+import com.natamus.improvedsignediting.data.Constants;
+import com.natamus.improvedsignediting.events.SignEditEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(value = Dist.CLIENT)
+public class NeoForgeSignEditEvent {
+	@SubscribeEvent
+	public static void onClientTick(ClientTickEvent e) {
+
+		SignEditEvent.onClientTick(Constants.mc);
+	}
+}
