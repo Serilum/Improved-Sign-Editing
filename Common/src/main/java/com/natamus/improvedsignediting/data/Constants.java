ce7b7d0618b824f09505830a261199fac27c9ab0
Rick South <rick@natamus.com>
2024-05-02 11:10:45 +0200

    (v1.3) Fixed the mod not working with hanging signs. [+1]

--- Common/src/main/java/com/natamus/improvedsignediting/data/Constants.java
+++ Common/src/main/java/com/natamus/improvedsignediting/data/Constants.java
@@ -2,9 +2,13 @@ package com.natamus.improvedsignediting.data;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.Screen;
 
 public class Constants {
     public static final Minecraft mc = Minecraft.getInstance();
     public static final Font font = mc.font;
-    public static int maxSignWidth = 85;
+
+    public static int getMaxSignWidth(Screen screen) {
+        return 85;
+    }
 }
