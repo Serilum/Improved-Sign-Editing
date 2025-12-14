973df3b7f4dbd6a133e942fd5d827269df2ef7cc
Rick South <rick@natamus.com>
2025-12-14 14:54:04 +0100

    Improved Sign Editing source code for 1.21.11.

---
+++ Forge/src/main/java/com/natamus/improvedsignediting/forge/events/ForgeSignEditEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.improvedsignediting.forge.events;
+
+import com.natamus.improvedsignediting.data.Constants;
+import com.natamus.improvedsignediting.events.SignEditEvent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeSignEditEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeSignEditEvent.class);
+
+		ClientTickEvent.Pre.BUS.addListener(ForgeSignEditEvent::onClientTick);
+	}
+
+	@SubscribeEvent
+	public static void onClientTick(ClientTickEvent.Pre e) {
+		SignEditEvent.onClientTick(Constants.mc);
+	}
+}
